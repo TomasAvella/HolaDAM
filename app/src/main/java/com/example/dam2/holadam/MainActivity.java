c11820e0f66aa54f1f87348de21730ccd80153ff
Tomas Avella <tomasavella007@gmail.com>
2016-09-29 15:31:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dam2/holadam/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dam2.holadam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        TextView nuevotexto = new TextView(this);
+        nuevotexto.setText("Hola DAM desde Java");
+        setContentView(nuevotexto)
+    }
+}
